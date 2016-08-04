0f1691fd0e8b465f3a24664a0810efd39e50f6b3
watanabe <watanabe.masahiro@nttcom.co.jp>
2016-08-04 10:41:27 +0900

    refactoring by using array

--- chevot_arduino.ino
+++ chevot_arduino.ino
@@ -1,25 +1,9 @@
-const int button1 = 14;
-const int button2 = 16;
-const int button3 = 18;
-//表示する数字
-int num1 = 0;
-int num2 = 0;
-int num3 = 0;
-//本当の数字
-int renum1 = 0;
-int renum2 = 0;
-int renum3 = 0;
-//数字の入れ替えで使用
-int numch = 0;
-int chch = 0;
-//数字と投票先の関係を記憶(文字にしたほうが混乱ないかも)
-int a = 1;
-int b = 2;
-int c = 3;
-//LEDのダイナミック点灯フラグ
-boolean _flg = false;
-//チャタリング防止
-int cnt = 0;
+int button_pins[3] = {14, 16, 18};
+int display_votes[3] = {0, 0, 0};
+int real_display_map[3] = {0, 1, 2}; // 「実際の投票数」が「表示している投票数」のどれに当たるかを示すindexのマップ。real_display_map[0]の値がこだわりのindexでdisplay_votes[real_display_map[0]] がその投票数
+
+boolean led_dynamic_flag = false;
+int chattering_counter = 0;
 
 void setup() {
   Serial.begin(9600); 
@@ -30,17 +14,18 @@ void setup() {
   for (int i = 30; i <= 38; i++) {
     pinMode(i, OUTPUT);
   }
-   for (int i = 58; i <= 66; i++) {
+  for (int i = 58; i <= 66; i++) {
     pinMode(i, OUTPUT);
   }
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  pinMode(button3, INPUT);
+  for (int i = 0; i <= 3; i++) {
+    pinMode(button_pins[i], INPUT);
+  }
   pinMode(15, OUTPUT);
   pinMode(17, OUTPUT);
   pinMode(19, OUTPUT);
   //スイッチへの電源供給
 }
+
 //LEDレイアウトを定義
 boolean Num_Array[10][7] = {
   {0, 0, 1, 0, 0, 0, 0}, //0
@@ -55,22 +40,13 @@ boolean Num_Array[10][7] = {
   {1, 0, 0, 0, 0, 0, 0} //9
 };
 
-//LED表示関数を定義
-void NumPrint1(int Number) {
-  for (int w = 0; w <= 6; w++) {
-    digitalWrite(w + 58, -Num_Array[Number][w]);
-  }
+void NumPrint(int index, int Number) {
+    int offset[] = {58, 30, 4};
+    for (int w = 0; w < 7; w++) {
+      digitalWrite(w + offset[index], -Num_Array[Number][w]);
+    }
 }
-void NumPrint2(int Number) {
-  for (int w = 0; w <= 6; w++) {
-    digitalWrite(w + 30, -Num_Array[Number][w]);
-  }
-}
-void NumPrint3(int Number) {
-  for (int w = 0; w <= 6; w++) {
-    digitalWrite(w + 4, -Num_Array[Number][w]);
-  }
-}
+
 //2桁をそれぞれの桁で分解する関数
 int NumParse(int Number, int s) {
   if (s == 1) {
@@ -81,240 +57,76 @@ int NumParse(int Number, int s) {
   }
   return 0;
 }
-void loop() {
-  if (cnt == 41) {
-    cnt = 0;
 
+void debug_print_to_serial() {
+    String message[3] = { "こだわりは", "凡例１は", "凡例2は" };
+    for(int i=0; i<3; i++) {
+      Serial.print(message[i]);
+      Serial.println(display_votes[real_display_map[i]]);
+    }
+}
+
+void cheat_swap_highest_score() {
+  int max_index = 0;
+  int max_votes = 0;
+  for(int i=0; i<3; i++) {
+    if (display_votes[i] > max_votes) {
+       max_index = i;
+       max_votes = display_votes[i];
+    }
   }
-  digitalWrite(15, HIGH);
+  int fanantial_index = real_display_map[0];
+  if (fanantial_index == max_index) {
+     return;
+  }
+  int tmp = display_votes[max_index];
+  display_votes[max_index] = display_votes[fanantial_index];
+  display_votes[fanantial_index] = tmp;
+  tmp = real_display_map[max_index];
+  real_display_map[max_index] = real_display_map[fanantial_index]; 
+  real_display_map[fanantial_index] = tmp;
+}
+
+void loop() {
+  digitalWrite(15, HIGH); // → setup()に持っていっても良い?
   digitalWrite(17, HIGH);
   digitalWrite(19, HIGH);
   //スイッチへの電源供給
 
-  //flg変数をtrue / falseを切り替えることで11番13番ピンの出力を交互に切り替える
-  if (_flg) {
+  //led_dynamic_flag変数をtrue / falseを切り替えることで11番13番ピンの出力を交互に切り替える
+  if (led_dynamic_flag) {
     digitalWrite(11, LOW);
     digitalWrite(12, HIGH);
     digitalWrite(37, LOW);
     digitalWrite(38, HIGH);
     digitalWrite(65, LOW);
     digitalWrite(66, HIGH);
-    _flg = false;
-    NumPrint1(NumParse(num1, 1)); //1桁目の表示
-    NumPrint2(NumParse(num2, 1)); //1桁目の表示
-    NumPrint3(NumParse(num3, 1)); //1桁目の表示
-  }
-  else {
-    digitalWrite(12, LOW);
+    for(int i=0; i<3; i++) {
+      NumPrint(i, NumParse(display_votes[i], 1)); //1桁目の表示
+    }
+  } else {
     digitalWrite(11, HIGH);
-    digitalWrite(38, LOW);
+    digitalWrite(12, LOW);
     digitalWrite(37, HIGH);
-    digitalWrite(66, LOW);
+    digitalWrite(38, LOW);
     digitalWrite(65, HIGH);
-    _flg = true;
-    NumPrint1(NumParse(num1, 2)); //1桁目の表示
-    NumPrint2(NumParse(num2, 2)); //1桁目の表示
-    NumPrint3(NumParse(num3, 2)); //1桁目の表示
+    digitalWrite(66, LOW);
+    for(int i=0; i<3; i++) {
+      NumPrint(i, NumParse(display_votes[i], 2)); //2桁目の表示
+    }
   }
-  //ここからボタンの読み取りについて
-  //
-  //
-  cnt++;
-  if (cnt == 40) {//ボタンのチャタリング防止
-    
-    //ここからボタン1
-    if (digitalRead (button1) == HIGH) {
-      num1++;
-      if (a == 1) {
-        renum1++;
+  led_dynamic_flag = !led_dynamic_flag;
+  chattering_counter++;
+  if (chattering_counter == 40) {//ボタンのチャタリング防止
+    for (int i=0; i<3; i++) {
+      //ここからボタン i
+      if (digitalRead(button_pins[i]) == HIGH) {
+        display_votes[i]++;
+        cheat_swap_highest_score();
+        debug_print_to_serial();
       }
-      if (a == 2) {
-        renum2++;
-        if (b == 1) {
-          if (num1 > num2) {
-            numch = num1;
-            num1 = num2;
-            num2 = numch;
-            chch = a;
-            a = b;
-            b = chch;
-            
-
-          }//numは数字abcはnumと投票先の関係を記憶
-          if (c == 1) {
-            if (num1 > num3) {
-              numch = num1;
-              num1 = num3;
-              num3 = numch;
-              chch = a;
-              a = c;
-              c = chch;
-            }
-          }
-        }
-      }
-      if (a == 3) {
-        renum3++;
-        if (b == 1) {
-          if (num1 > num2) {
-            numch = num1;
-            num1 = num2;
-            num2 = numch;
-            chch = a;
-            a = b;
-            b = chch;
-          }
-        }
-        if (c == 1) {
-          if (num1 > num3) {
-            numch = num1;
-            num1 = num3;
-            num3 = numch;
-            chch = a;
-            a = c;
-            c = chch;
-          }
-        }
-
-      }
-
-      Serial.print("こだわりは");
-      Serial.println(renum1);
-      Serial.print("凡例１は");
-      Serial.println(renum2);
-      Serial.print("凡例2は");
-      Serial.println(renum3);
     }
-
-    //ここからボタン2
-    if (digitalRead(button2) == HIGH) {
-      num2++;
-      if (b == 1) {
-        renum1++;
-      }
-      if (b == 2) {
-        renum2++;
-        if (a == 1) {
-          if (num2 > num1) {
-            numch = num2;
-            num2 = num1;
-            num1 = numch;
-            chch = b;
-            b = a;
-            a = chch;
-          }
-          if (c == 1) {
-            if (num2 > num3) {
-              numch = num2;
-              num2 = num3;
-              num3 = numch;
-              chch = b;
-              b = c;
-              c = chch;
-            }
-          }
-        }
-      }
-      if (b == 3) {
-        renum3++;
-        if (a == 1) {
-          if (num2 > num1) {
-            numch = num2;
-            num2 = num1;
-            num1 = numch;
-            chch = b;
-            b = a;
-            a = chch;
-          }
-        }
-        if (c == 1) {
-          if (num2 > num3) {
-            numch = num2;
-            num2 = num3;
-            num3 = numch;
-            chch = b;
-            b = c;
-            c = chch;
-          }
-        }
-
-      }
-      Serial.print("こだわりは");
-      Serial.println(renum1);
-
-      Serial.print("凡例１は");
-      Serial.println(renum2);
-
-      Serial.print("凡例2は");
-      Serial.println(renum3);
-    }
-
-
-    //ここからボタン3
-    if (digitalRead(button3) == HIGH) {
-      num3++;
-      if (c == 1) {
-        renum1++;
-      }
-      if (c == 2) {
-        renum2++;
-        if (a == 1) {
-          if (num3 > num1) {
-            numch = num3;
-            num3 = num1;
-            num1 = numch;
-            chch = c;
-            c = a;
-            a = chch;
-          }
-          if (c == 1) {
-            if (num3 > num2) {
-              numch = num3;
-              num3 = num2;
-              num2 = numch;
-              chch = c;
-              c = b;
-              b = chch;
-            }
-          }
-        }
-      }
-      if (c == 3) {
-        renum3++;
-        if (a == 1) {
-          if (num3 > num1) {
-            numch = num3;
-            num3 = num1;
-            num1 = numch;
-            chch = c;
-            c = a;
-            a = chch;
-          }
-        }
-        if (b == 1) {
-          if (num3 > num2) {
-            numch = num3;
-            num3 = num2;
-            num2 = numch;
-            chch = c;
-            c = b;
-            b = chch;
-          }
-        }
-
-      }
-
-      Serial.print("こだわりは");
-      Serial.println(renum1);
-
-      Serial.print("凡例１は");
-      Serial.println(renum2);
-
-      Serial.print("凡例2は");
-      Serial.println(renum3);
-
-    }
-    cnt = 0;
+    chattering_counter = 0;
   }
   delay(5);
 }
