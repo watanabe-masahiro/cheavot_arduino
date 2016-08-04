953483d6d54769df81658d49d073e4f27c3c793a
watanabe <watanabe.masahiro@nttcom.co.jp>
2016-08-04 11:24:33 +0900

    debug

--- chevot_arduino.ino
+++ chevot_arduino.ino
@@ -33,7 +33,7 @@ void setup() {
   for (int i = 58; i <= 66; i++) {
     pinMode(i, OUTPUT);
   }
-  for (int i = 0; i <= 3; i++) {
+  for (int i = 0; i < 3; i++) {
     pinMode(button_pins[i], INPUT);
   }
   pinMode(15, OUTPUT);
